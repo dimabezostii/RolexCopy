4f904d7a15244ac8d30e4c78866ff823abca4bd7
Dima <dimabezostii2@gmail.com>
2022-01-19 23:39:47 +0100

    header update - toggle elements

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,87 @@ window.addEventListener('resize', () => {
     watchContainer.style.height = parseInt(scrollWatchContainer.offsetHeight + 5) + 'px';
 });
 // PANE SCROLLBAR POSITION - END
+
+
+
+
+
+
+
+
+
+// HTML, BODY VARIABLES
+const body = document.querySelector('body');
+
+// PANE TOGGLE START -----------------------------------------------------------------------------------------------------------------------------------
+const menuBtn = document.querySelector('#menu');
+const pane = document.querySelector('#pane');
+const closePane = document.querySelector('.top>i');
+const header = document.querySelector('header');
+
+menuBtn.addEventListener('click', () => {
+    pane.classList.add('active');
+    overlay.classList.add('active');
+    header.classList.add('hide');
+    body.classList.add('blockScroll');
+});
+
+closePane.addEventListener('click', () => {
+    pane.classList.remove('active');
+    overlay.classList.remove('active');
+    header.classList.remove('hide');
+    body.classList.remove('blockScroll');
+});
+// PANE TOGGLE END
+
+// RECHERCHE PANE TOGGLE START ------------------------------------------------------------------------------------------------------------------------
+const recherche = document.querySelector('#recherche');
+const searchPane = document.querySelector('.search-pane');
+const closeRecherche = document.querySelector('.search-pane-close');
+
+recherche.addEventListener('click', () => {
+    overlay.classList.add('active');
+    searchPane.classList.add('show');
+    header.classList.add('hide');
+    body.classList.add('blockScroll');
+});
+
+closeRecherche.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    searchPane.classList.remove('show');
+    header.classList.remove('hide');
+    body.classList.remove('blockScroll');
+});
+// RECHERCHE PANE TOGGLE END
+
+
+// SELECTION PANE TOGGLE START ------------------------------------------------------------------------------------------------------------------------
+const selectionLink = document.querySelector('#selectionLink');
+const selectionPane = document.querySelector('.selection-pane');
+const closeSelection = document.querySelector('.selection-pane-close-btn');
+
+selectionLink.addEventListener('click', () => {
+    overlay.classList.add('active');
+    selectionPane.classList.add('show');
+    header.classList.add('hide');
+    body.classList.add('blockScroll');
+});
+
+closeSelection.addEventListener('click', () => {
+    overlay.classList.remove('active');
+    selectionPane.classList.remove('show');
+    header.classList.remove('hide');
+    body.classList.remove('blockScroll');
+});
+// SELECTION PANE TOGGLE END
+
+
+// OVERLAY VARIABLE
+const overlay = document.querySelector('.pane-overlay');
+overlay.addEventListener('click', () => {
+    pane.classList.remove('active');
+    overlay.classList.remove('active');
+    header.classList.remove('hide');
+    searchPane.classList.remove('show');
+    body.classList.remove('blockScroll');
+});
